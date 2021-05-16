26bdae47ff9e61e8d1e005faff59c28d70afdcce
Luiz Paulo de Oliveira <lpdopaulo@gmail.com>
2021-05-16 16:25:32 -0300

    criado o modulo de personalizada!

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,6 +4,7 @@ import { RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   { path: 'home', loadChildren: () => import('./home/home.module').then(m => m.HomeModule) },
   { path: 'produtos', loadChildren: () => import('./produto/produto.module').then(m => m.ProdutoModule) },
+  { path: 'personalizada', loadChildren: () => import('./personalizada/personalizada.module').then(m => m.PersonalizadaModule) },
   { path: '**', redirectTo: '/home' }
 ];
 
---
+++ src/app/personalizada/personalizada.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-personalizada',
+  templateUrl: './personalizada.component.html',
+  styleUrls: ['./personalizada.component.css']
+})
+export class PersonalizadaComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
